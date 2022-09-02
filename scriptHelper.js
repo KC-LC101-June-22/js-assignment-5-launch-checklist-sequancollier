8e4a3cfac5439610ff661760aba240f040ba6541
Sequan Collier <sequancollier@gmail.com>
2022-09-01 23:17:58 -0500

    fixed the validations issue

--- scriptHelper.js
+++ scriptHelper.js
@@ -49,13 +49,13 @@ function formSubmission (document, list, pilotName, copilotName, fuelLevel, carg
     let copilotStatus = document.getElementById('copilotStatus');
     
 
-    if (validateInput(pilotName) === `Empty`|| validateInput(copilotName) === `Empty`|| 
-    validateInput(fuelLevel) === `Empty`||validateInput(cargoLevel) === `Empty`) {
-        alert(`All fields are required`);
+    if (validateInput(pilotName) === 'Empty'|| validateInput(copilotName) === 'Empty'|| 
+    validateInput(fuelLevel) === 'Empty'||validateInput(cargoLevel) === 'Empty') {
+        alert('All fields are required');
     }
     else if (validateInput(fuelLevel) === 'Not a Number' || validateInput(cargoLevel) === 'Not a Number') {
-        alert(`Please enter numerical values for Fuel Level and Cargo Mass`);
-    } else if (validateInput(pilotName)===`Is a Number`||validateInput(copilotName)===`Is a Number`) {
+        alert('Please enter numerical values for Fuel Level and Cargo Mass');
+    } else if (validateInput(pilotName)==='Is a Number'||validateInput(copilotName)==='Is a Number') {
         alert('Please do not enter numbers for name of pilot or co-pilot');
     } 
     else {
@@ -64,21 +64,21 @@ function formSubmission (document, list, pilotName, copilotName, fuelLevel, carg
     list.style.visibility = 'hidden';
     }
     if (Number(fuelLevel) < 10000) {
-        fuelStatus.innerHTML = `There is not enough fuel for journey`;
+        fuelStatus.innerHTML = 'There is not enough fuel for journey';
         list.style.visibility = 'visible';
-        launchStatus.innerHTML = `Thr Shuttle not ready for launch`;
-        launchStatus.style.color = `red`;
+        launchStatus.innerHTML = 'Thr Shuttle not ready for launch';
+        launchStatus.style.color = 'red';
     } else if (Number(cargoLevel) > 10000) {
-        cargoStatus.innerHTML = `There is too much mass for the shuttle to take off.`;
-        list.style.visibility = `visible`;
-        launchStatus.innerHTML = `The Shuttle not ready for launch`;
-        launchStatus.style.color = `red`;
+        cargoStatus.innerHTML = 'There is too much mass for the shuttle to take off.';
+        list.style.visibility = 'visible';
+        launchStatus.innerHTML = 'The Shuttle not ready for launch';
+        launchStatus.style.color = 'red';
     } else if (Number(cargoLevel) < 10000 && Number(fuelLevel) > 10000) {
-        list.style.visibility = `visible`;
-        fuelStatus.innerHTML = `We have enough fuel for journey`;
-        cargoStatus.innerHTML = `Cargo is at an approitate weight for liftoff`;
-        launchStatus.innerHTML = `The Shuttle ready for launch`;
-        launchStatus.style.color = `green`;
+        list.style.visibility = 'visible';
+        fuelStatus.innerHTML = 'We have enough fuel for journey';
+        cargoStatus.innerHTML = 'Cargo is at an approitate weight for liftoff';
+        launchStatus.innerHTML = 'The Shuttle ready for launch';
+        launchStatus.style.color = 'green';
     }
 
 }
